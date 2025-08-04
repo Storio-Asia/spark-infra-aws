ee8dadcd62584133d3a3c6ca72510e11e4ec3054
Hassan <hassanbaber@gmail.com>
2025-08-04 21:02:43 +1000

    db route

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,7 +101,7 @@ resource "aws_route_table" "db-rt" {
 }
 
 resource "aws_route" "db_internet_route" {
-  route_table_id         = aws_route_table.db-rt
+  route_table_id         = aws_route_table.db-rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
