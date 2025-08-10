b9a90ddf957692b051d7e90f8e2a509dc38b6cb8
Hassan Sheikh <hsheikh@powercor.com.au>
2025-08-11 08:11:13 +1000

    helm provider added

--- terraform/main.tf
+++ terraform/main.tf
@@ -542,4 +542,16 @@ resource "kubernetes_service_account" "alb_controller" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
 ##############################################################################################
