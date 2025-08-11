9d98c51855dd4430faef3a340ab39cce03b0bd77
Hassan Sheikh <hsheikh@powercor.com.au>
2025-08-12 09:35:29 +1000

    this should be it for k8s

--- terraform/modules/EKSClusterAutoScaler/providers.tf
+++ terraform/modules/EKSClusterAutoScaler/providers.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
+  host                   = var.eks_cluster_endpoint
   cluster_ca_certificate =  base64decode(var.cluster_certificate_authority_data)# base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
