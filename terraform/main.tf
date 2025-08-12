49776f380a5d365a6c9fca5c8bf4ce860d4efaf8
Hassan Sheikh <hsheikh@powercor.com.au>
2025-08-12 10:54:15 +1000

    provider main block

--- terraform/main.tf
+++ terraform/main.tf
@@ -461,12 +461,7 @@ module "eks"{
   
 }
 
-provider "kubernetes" {
-  alias = "k8s"
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate =  base64decode(module.eks.cluster_certificate_authority_data)# base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
+
 
 ############# Load balancer module. Sets up ELB, its Iam role, SA account and install it via helm ################################
 module "elb"{
@@ -477,7 +472,7 @@ module "elb"{
   eks_oidc_provider_arn = module.eks.oidc_provider_arn
   cluster_certificate_authority_data = module.eks.cluster_certificate_authority_data
   providers = {
-    kubernetes = kubernetes.k8s
+    kubernetes = kubernetes
   }
 }
 
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -16,5 +16,11 @@ terraform {
       source = "hashicorp/helm"
     }
   }
+}
 
+provider "kubernetes" {
+  #alias = "k8s"
+  host   = module.eks.cluster_endpoint
+  cluster_ca_certificate =  base64decode(module.eks.cluster_certificate_authority_data)# base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
