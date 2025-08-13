fce1c113db1cf1953c32f64fe805a0bfc00cd0ee
Hassan Sheikh <hsheikh@powercor.com.au>
2025-08-13 14:55:01 +1000

    updated policy

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -80,7 +80,7 @@ resource "aws_iam_role" "vpc_cni" {
           "${module.eks.oidc_provider}:sub": "system:serviceaccount:kube-system:aws-node",
           "aws:SourceArn" = "arn:aws:eks:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:cluster/${module.eks.cluster_name}"
         },
-        StringLike = {
+        "StringLike" : {
                 "aws:SourceIdentity" = "system:serviceaccount:kube-system:aws-node"
               }
       }
